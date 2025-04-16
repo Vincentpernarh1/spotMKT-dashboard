070cca842c00575ce7595501e4c3157c7d7ceef2
Vincent Pernarh <seu-email@example.com>
2025-04-16 19:53:32 -0300

    setting up vite for deploymento to vercel

--- spot-dashboard/vite.config.js
+++ spot-dashboard/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // optional, for cleaner imports like '@/components/Button'
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // 👈 this is the key!
+  },
 })
